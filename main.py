79df24539aed0bedabd797013df0a67f8d265199
Rajat Singla <rajat.singla@toptal.com>
2023-01-18 19:23:22 +0530

    yo1

--- main.py
+++ main.py
@@ -31,7 +31,8 @@ app.add_middleware(
 
 # SqlAlchemy Setup
 load_dotenv()
-DB_URL = os.getenv('DB_URL')
+print(os.environ.get('DB_URL'))
+DB_URL = os.environ.get('DB_URL')
 engine = create_engine(DB_URL, echo=True, future=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
